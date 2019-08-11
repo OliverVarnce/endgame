200bb2b2631661b6514802e8dcfffc7d0a9459bf
Oleh Voitenko <ovoitenko@e2r13p6.unit.ua>
2019-08-11 15:49:12 +0300

    popytka dobavit back

--- src/main.c
+++ src/main.c
@@ -31,14 +31,22 @@ int main () {
 		
 		return 0;
 	}
+/*------------------------- Background ------------------------*/
+	int flags=IMG_INIT_JPG|IMG_INIT_PNG;
+ 	int initted=IMG_Init(flags);
+
+ 	if(initted & flags != flags) {
+     printf("IMG_Init: Failed to init required jpg and png support!\n");
+     printf("IMG_Init: %s\n", IMG_GetError());
+	}
 
 	SDL_Surface *background;
- 	background = IMG_Load("./sources/water2.gif");
+ 	background = IMG_LoadTyped_RW(SDL_RWFromFile("./sources/water2.gif", "gif"), 1, "GIF");
 
  	if(!background)
     	printf("IMG_Load: %s\n", IMG_GetError());
 
-
+/*-----------------------------------------------------------*/
 
 
 

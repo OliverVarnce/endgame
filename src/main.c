363fca8fae54f7c954dc520ac6e8589226d65fab
Andrii Hrynko <ahrynko@e2r13p8.unit.ua>
2019-08-12 18:46:30 +0300

    collisons

--- src/main.c
+++ src/main.c
@@ -11,13 +11,19 @@ const int SCREEN_HEIGHT = 800;
 int main() {
     int running = 1;
     SDL_Event event;
+
+    // int x;
+    // int y = 0;
     
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow("SAVE THE OCEAN", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
     SDL_Surface *surface = SDL_GetWindowSurface(window);  // создает sufrace для всего окна
-    SDL_Surface *img = IMG_Load("resources/water2.gif");
+    SDL_Surface *img = IMG_Load("resources/water.jpg");
     SDL_Surface *ship = IMG_Load("resources/ship.png"); // Эта функция загружвет изображение с любым расширением
+    SDL_Surface *trash = IMG_Load("resources/trash.jpg");
     SDL_Rect rect = {0, 0, 0, 0}; // создаем прямоугольник с картинкой, которую будем вставлять. Первые две переменные x,y это начальные точки на экране  {x, y, h, w}
+    SDL_Rect shp = {0, 0, 0, 0};
+    SDL_Rect t = {500, 500, 0, 0};
 
     if (NULL == window)
         exit (1);
@@ -28,9 +34,23 @@ int main() {
         {
             if((SDL_QUIT == event.type) || (SDL_KEYDOWN == event.type && SDL_SCANCODE_ESCAPE == event.key.keysym.scancode))
                 running = 0;
+            if (event.type == SDL_KEYDOWN && SDL_SCANCODE_UP == event.key.keysym.scancode)
+                shp.y -= 10;
+            if (event.type == SDL_KEYDOWN && SDL_SCANCODE_DOWN == event.key.keysym.scancode)
+                shp.y += 10;
+            if (event.type == SDL_KEYDOWN && SDL_SCANCODE_LEFT == event.key.keysym.scancode)
+                shp.x -= 10;
+            if (event.type == SDL_KEYDOWN && SDL_SCANCODE_RIGHT == event.key.keysym.scancode)
+                shp.x += 10;
+
         }
+
+        if(SDL_HasIntersection(&shp, &t) == SDL_TRUE)
+            running = 0;
+
         SDL_BlitSurface(img, NULL, surface, &rect); // вставляет картинку
-        SDL_BlitSurface(ship, NULL, surface, &rect); // вставляет картинку
+        SDL_BlitSurface(ship, NULL, surface, &shp);
+        SDL_BlitSurface(trash, NULL, surface, &t); // вставляет картинку
         SDL_UpdateWindowSurface(window);
     }
     
